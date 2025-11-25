af833ab73759a04119420e0aceacbd342df29e3e
elflee77 <elflee77@gmail.com>
2025-11-25 11:09:18 +0900

    .

---
+++ Assets/Script/Accelerator.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Accelerator : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 TorchDir = value.Get<Vector2>();
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddTorque(new Vector3(TorchDir.y * 10, 0, -TorchDir.x * 10));
+
+        Debug.Log("OnMove called");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        
+
+    }
+}
